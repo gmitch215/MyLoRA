b54494c16c84b2453f706a39f914d8f344ea8c6f
Gregory Mitchell <54124162+gmitch215@users.noreply.github.com>
2026-06-28 20:14:49 -0500

    chore: make server types reference top-level types

--- src/server/types/auth.d.ts
+++ src/server/types/auth.d.ts
@@ -1,18 +1,1 @@
-declare module '#auth-utils' {
-	interface User {
-		id: string;
-		username: string;
-		displayName: string;
-		role: 'administrator' | 'manager' | 'developer';
-		avatarPathname?: string | null;
-		bio?: string | null;
-	}
-
-	interface UserSession {
-		loggedInAt: number;
-	}
-
-	interface SecureSessionData {}
-}
-
-export {};
+/// <reference path="../../types/auth.d.ts" />
